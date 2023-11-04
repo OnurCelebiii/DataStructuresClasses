37556911c3b0fcb4ec8c7860fcf8325a7fbba89e
Clang Format <clang-format@karaman.dev>
2023-11-04 09:55:20 +0000

    apply clang-format

--- tst/LinkedList_test.cpp
+++ tst/LinkedList_test.cpp
@@ -1,31 +1,26 @@
-#include <gtest/gtest.h>
 #include "LinkedList.h"
+#include <gtest/gtest.h>
 
-TEST(OnurTest, isEmpty_false)
-{
-    LinkedList<int> myList;
+TEST(OnurTest, isEmpty_false) {
+  LinkedList<int> myList;
 
-    myList.push_back(1);
-    myList.push_back(2);
+  myList.push_back(1);
+  myList.push_back(2);
 
-    ASSERT_EQ(false, myList.isEmpty());
+  ASSERT_EQ(false, myList.isEmpty());
 }
 
+TEST(OnurTest, isEmpty_true) {
+  LinkedList<int> myList;
 
-TEST(OnurTest, isEmpty_true)
-{
-    LinkedList<int> myList;
+  myList.push_back(1);
+  myList.deleteFirst();
+  myList.deleteFirst();
 
-    myList.push_back(1);
-    myList.deleteFirst();
-    myList.deleteFirst();
-    
-    ASSERT_EQ(true, myList.isEmpty());
+  ASSERT_EQ(true, myList.isEmpty());
 }
 
-
-int main(int argc, char **argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }

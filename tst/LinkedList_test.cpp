d567a77611ce9777f3747c8927ac5b0043902512
Clang Format <clang-format@karaman.dev>
2023-11-04 10:40:06 +0000

    apply clang-format

--- tst/LinkedList_test.cpp
+++ tst/LinkedList_test.cpp
@@ -1,8 +1,7 @@
 #include "LinkedList.h"
 #include <gtest/gtest.h>
 
-TEST(isEmpty, false)
-{
+TEST(isEmpty, false) {
   LinkedList<int> myList;
 
   myList.push_back(1);
@@ -11,8 +10,7 @@ TEST(isEmpty, false)
   ASSERT_EQ(false, myList.isEmpty());
 }
 
-TEST(isEmpty, true)
-{
+TEST(isEmpty, true) {
   LinkedList<int> myList;
 
   myList.push_back(1);
@@ -22,15 +20,13 @@ TEST(isEmpty, true)
   ASSERT_EQ(true, myList.isEmpty());
 }
 
-TEST(isEmpty, initial)
-{
+TEST(isEmpty, initial) {
   LinkedList<int> myList;
 
   ASSERT_EQ(true, myList.isEmpty());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
